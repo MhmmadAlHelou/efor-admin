a8288db16951431f26b26f298df9d57ad8032cff
MhmmadAlHelou <139993356+MhmmadAlHelou@users.noreply.github.com>
2026-05-31 18:27:35 +0300

    Update view_items.dart

--- lib/view/screen/items/view_items.dart
+++ lib/view/screen/items/view_items.dart
@@ -8,88 +8,12 @@ import 'package:get/get.dart';
 class ItemsViewCard extends StatelessWidget {
   const ItemsViewCard({super.key});
 
-  // @override
-  // Widget build(BuildContext context) {
-  //   Get.put(ItemsViewCardController());
-  //   return Scaffold(
-  //     appBar: AppBar(title: const Text('Items')),
-  //     body: GetBuilder<ItemsViewCardController>(
-  //       builder: (controller) => HandlingDataView(
-  //         statusRequest: controller.statusRequest,
-  //         widget:
-  //             //  WillPopScope(
-  //             //   onWillPop: () => controller.goBack(),
-  //             // child:
-
-  //             Padding(
-  //           padding: const EdgeInsets.symmetric(horizontal: 10),
-  //           child: ListView.builder(
-  //             itemCount: controller.data.length,
-  //             itemBuilder: (context, index) => controller
-  //                         .data[index].categoriesName ==
-  //                     controller.categoriesModel.categoriesName
-  //                 ? InkWell(
-  //                     onTap: () {
-  //                       controller.goToEditPage(controller.data[index]);
-  //                     },
-  //                     child: Card(
-  //                       child: Row(
-  //                         children: [
-  //                           // if (controller.data[index].categoriesName ==
-  //                           //     controller.categoriesModel.categoriesName)
-  //                           Expanded(
-  //                               child: Padding(
-  //                             padding: const EdgeInsets.all(4),
-  //                             child: CachedNetworkImage(
-  //                                 height: 80,
-  //                                 imageUrl:
-  //                                     "${AppLink.imageItems}/${controller.data[index].categoriesName}/${controller.data[index].itemsImage}"),
-  //                           )),
-  //                           Expanded(
-  //                               flex: 3,
-  //                               child: ListTile(
-  //                                 title:
-  //                                     Text(controller.data[index].itemsName!),
-  //                                 subtitle: Text(
-  //                                     "${controller.data[index].itemsPrice}\$"),
-  //                                 trailing: Row(
-  //                                   mainAxisSize: MainAxisSize.min,
-  //                                   children: [
-  //                                     IconButton(
-  //                                       onPressed: () {
-  //                                         controller.deleteItem(
-  //                                             controller.data[index].itemsId
-  //                                                 .toString(),
-  //                                             controller
-  //                                                 .data[index].itemsImage!,
-  //                                             controller
-  //                                                 .data[index].categoriesName!);
-  //                                       },
-  //                                       icon: const Icon(Icons.delete_outline),
-  //                                     ),
-  //                                   ],
-  //                                 ),
-  //                               )),
-  //                         ],
-  //                       ),
-  //                     ),
-  //                   )
-  //                 : const SizedBox(height: 1),
-  //           ),
-  //         ),
-  //         // ),
-  //       ),
-  //     ),
-  //   );
-  // }
-
   @override
   Widget build(BuildContext context) {
     Get.put(ItemsViewCardController());
     return GetBuilder<ItemsViewCardController>(
       builder: (controller) => Scaffold(
         appBar: AppBar(title: Text(
-            // "${controller.data[controller.categoriesModel.categoriesId!].categoriesName}")),
             "${controller.categoriesModel.categoriesName}")),
         body: HandlingDataView(
           statusRequest: controller.statusRequest,
